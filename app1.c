c1e6704d271648b5b4642124d077736eccad4e8c
aybehrouz <behrouz_ayati@yahoo.com>
2021-09-29 15:06:48 +0330

    Save stack overflow handler code
    Signed-off-by: aybehrouz <behrouz_ayati@yahoo.com>

--- app1.c
+++ app1.c
@@ -4,24 +4,23 @@
 
 int fib(int n) {
     if (n <= 1) return n;
-    return fib(n - 1) + fib(n - 2);
+    return fib(n - 1) + 1;
 }
 
 int dispatcher(void* session, string_t request) {
     enter_area(session);
     string_t str = String("Hello!!!  new");
     string_buffer buf = StringBuffer(100);
-
     append_str(&buf, str);
     append_int64(&buf, 45);
     append_int64(&buf, 123456);
     printf("%s :: %d\n", buf.buffer, buf.end);
 
     loadInt64(session, 777);
-    printf("fib:%d\n", fib(10));
+    printf("fib:%d\n", fib(100000000));
     // for (int i = 0; i < 20000000000; ++i) {}
 
-    invoke_dispatcher(session, 220, 2, str);
+    //invoke_dispatcher(session, 220, 2, str);
     exit_area(session);
     return HTTP_OK;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -23,33 +23,58 @@ static void sig_handler(int sig, siginfo_t* info, void* ucontext) {
     }
 }
 
-void init_handlers() {
-    struct sigaction action{};
+void registerRecoveryStack() {
+    stack_t ss;
 
-    action.sa_flags = SA_SIGINFO;
-    action.sa_sigaction = sig_handler;
-    sigfillset(&action.sa_mask);
-    int err = 0;
-    err += sigaction(SIGALRM, &action, nullptr);
-    err += sigaction(SIGFPE, &action, nullptr);
-    err += sigaction(SIGUSR1, &action, nullptr);
-    err += sigaction(SIGSEGV, &action, nullptr);
-    err += sigaction(SIGBUS, &action, nullptr);
-    if (err != 0) {
-        printf("error in creating handlers\n");
-        exit(EXIT_FAILURE);
+    ss.ss_sp = malloc(SIGSTKSZ);
+    if (ss.ss_sp == nullptr) {
+        throw std::runtime_error("could not allocate memory for the recovery stack");
+    }
+    ss.ss_size = SIGSTKSZ;
+    ss.ss_flags = 0;
+    if (sigaltstack(&ss, nullptr) == -1) {
+        throw std::runtime_error("sigaltstack could not register the recovery stack");
     }
 }
 
+void init_handlers() {
+    struct sigaction action{};
+    action.sa_flags = SA_SIGINFO | SA_ONSTACK;
+    action.sa_sigaction = sig_handler;
+    sigfillset(&action.sa_mask);
+    int err = 0;
+    //err += sigaction(SIGALRM, &action, nullptr);
+    //err += sigaction(SIGFPE, &action, nullptr);
+    // err += sigaction(SIGUSR1, &action, nullptr);
+    err += sigaction(SIGSEGV, &action, nullptr);
+    // err += sigaction(SIGBUS, &action, nullptr);
+    if (err != 0) {
+        throw std::runtime_error("error in creating handlers");
+    }
+    printf("herrrreee!\n");
+}
+
+int foo() {
+    return foo();
+}
+
+void thtest() {
+    registerRecoveryStack();
+    foo();
+}
+
 int main(int argc, char const* argv[]) {
-    ascee::AppLoader::init(1);
-    ascee::AppLoader::init(2);
+    //ascee::AppLoader::init(1);
+    //ascee::AppLoader::init(2);
     init_handlers();
 
-    thread t1(executeSession, 111);
+    std::thread t(thtest);
+    t.join();
+
+    //thread t1(executeSession, 111);
     //thread t2(executeSession, 222);
 
-    t1.join();
+    //t1.join();
     //t2.join();
     return 0;
 }
