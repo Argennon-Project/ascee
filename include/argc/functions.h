2dfcc1f9e18da60d6ec4be18d38b1c276ba1252a
aybehrouz <behrouz_ayati@yahoo.com>
2021-10-12 15:38:32 +0330

    Add copyright
    Signed-off-by: aybehrouz <behrouz_ayati@yahoo.com>

--- include/argc/functions.h
+++ include/argc/functions.h
@@ -1,3 +1,20 @@
+// Copyright (c) 2021 aybehrouz <behrouz_ayati@yahoo.com>. All rights reserved.
+// This file is part of the C++ implementation of the Argennon smart contract
+// Execution Environment (AscEE).
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef ASCEE_ARGC_FUNCTIONS_H
 #define ASCEE_ARGC_FUNCTIONS_H
 
--- src/heap/Heap.h
+++ src/heap/Heap.h
@@ -1,3 +1,19 @@
+// Copyright (c) 2021 aybehrouz <behrouz_ayati@yahoo.com>. All rights reserved.
+// This file is part of the C++ implementation of the Argennon smart contract
+// Execution Environment (AscEE).
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef ASCEE_HEAP_H
 #define ASCEE_HEAP_H
@@ -7,7 +23,7 @@
 #include <vector>
 
 namespace ascee {
-//todo Heap must be signal-safe but it does not need to be thread-safe
+//TODO: Heap must be signal-safe but it does not need to be thread-safe
 class Heap {
 private:
     byte content[64] = {0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
